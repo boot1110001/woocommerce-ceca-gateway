65d504175cb08c9b912d210f4d8b322df1b54c70
boot1110001 <boot111.0001@gmail.com>
2019-08-26 09:53:33 +0200

    diferenciando las urls de pruebas y reales

--- woocommerce-ceca-gateway.php
+++ woocommerce-ceca-gateway.php
@@ -49,11 +49,12 @@ function woocommerce_gateway_ceca_init() {
             $this->currency     = $this->get_option( 'currency' );
             $this->language     = $this->get_option( 'language' );
             $this->debug        = $this->get_option( 'debug' ) == 'yes';
-            $this->ceca_url = 'https://pgw.ceca.es/cgi-bin/tpv';
 
             if($this->debug) {
+                $this->ceca_url = 'https://tpv.ceca.es:8000/cgi-bin/tpv';
                 $this->password     = $this->get_option( 'password_debug' );
             } else {
+                $this->ceca_url = 'https://pgw.ceca.es/cgi-bin/tpv';
                 $this->password     = $this->get_option( 'password' );
             }
 
